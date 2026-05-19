93498d0cd782681c6c6c98d62d0d6004f8b3e04f
prigodskiy <d.prigodskiy@gmail.com>
2026-05-20 03:35:10 +0700

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -8,19 +8,19 @@
 extern "C" {
 #endif
 
-#define STACK_MAX 100
+#define STACK_MAX_SIZE 100
 
 typedef struct {
-    int data[STACK_MAX];
-    int top;
+    int data[STACK_MAX_SIZE];
+    int top_index;
 } Stack;
 
-void StackInit(Stack* s);
-bool StackIsEmpty(Stack* s);
-bool StackPush(Stack* s, int x);
-bool StackPop(Stack* s);
-int StackTop(Stack* s);
-void StackClear(Stack* s);
+void stack_init(Stack* stack);
+bool stack_is_empty(const Stack* stack);
+bool stack_push(Stack* stack, int value);
+bool stack_pop(Stack* stack);
+int stack_top(const Stack* stack);
+void stack_clear(Stack* stack);
 
 #ifdef __cplusplus
 }
